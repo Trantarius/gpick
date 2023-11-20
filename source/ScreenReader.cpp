3b5430247bb802837741b3a6641208ab123efae8
Trantarius <leborjt@yahoo.com>
2023-11-20 11:24:08 -0500

    make ScreenReader hold an xdp screencast session

--- source/ScreenReader.cpp
+++ source/ScreenReader.cpp
@@ -20,21 +20,78 @@
 #include <gtk/gtk.h>
 #include <algorithm>
 #include <iostream>
+#include <libportal/portal.h>
+#include <pipewire/pipewire.h>
 struct ScreenReader {
 	cairo_surface_t *surface;
 	int maxSize;
 	GdkScreen *screen;
 	math::Rectangle<int> readArea;
+	XdpPortal* portal=nullptr;
+	XdpSession* session=nullptr;
+	pw_thread_loop* pwLoop=nullptr;
+	pw_context* pwContext=nullptr;
+	pw_core* pwCore=nullptr;
+	pw_stream* pwStream=nullptr;
 };
 struct ScreenReader *screen_reader_new() {
 	ScreenReader *screen = new ScreenReader;
 	screen->maxSize = 0;
 	screen->surface = 0;
 	screen->screen = 0;
+
+	pw_init(0,nullptr);
+	screen->portal = xdp_portal_new();
+	screen->session=nullptr;
+	GAsyncReadyCallback callback = [](GObject* source_object, GAsyncResult* res, gpointer callback_data){
+		ScreenReader* screen = (ScreenReader*)callback_data;
+		GError* error=nullptr;
+		screen->session=xdp_portal_create_screencast_session_finish(screen->portal,res,&error);
+		if(!screen->session || error){
+			std::cerr<<"Failed to create XDP screencast session";
+			if(error){
+				std::cerr<<": "<<error->message;
+				g_error_free(error);
+			}
+			std::cerr<<std::endl;
+		}else{
+			GAsyncReadyCallback start_callback = [](GObject* source_object, GAsyncResult* res, gpointer start_callback_data){
+				ScreenReader* screen = (ScreenReader*)start_callback_data;
+				GError* error=nullptr;
+				gboolean success = xdp_session_start_finish(screen->session,res,&error);
+				if(!success || error){
+					std::cerr<<"Failed to start XDP screencast session";
+					if(error){
+						std::cerr<<": "<<error->message;
+						g_error_free(error);
+					}
+					std::cerr<<std::endl;
+				}else{
+					screen->pwLoop = pw_thread_loop_new_full(nullptr,nullptr,new spa_dict());
+					screen->pwContext = pw_context_new(pw_thread_loop_get_loop(screen->pwLoop),nullptr,0);
+					screen->pwCore = pw_context_connect_fd(screen->pwContext,xdp_session_open_pipewire_remote(screen->session),nullptr,0);
+					screen->pwStream = pw_stream_new(screen->pwCore,nullptr,nullptr);
+					pw_stream_connect(screen->pwStream,PW_DIRECTION_INPUT,PW_ID_ANY,PW_STREAM_FLAG_AUTOCONNECT,nullptr,0);
+					pw_stream_set_active(screen->pwStream,true);
+					pw_thread_loop_start(screen->pwLoop);
+				}
+			};
+			xdp_session_start(screen->session,nullptr,nullptr,start_callback,screen);
+		}
+	};
+
+	xdp_portal_create_screencast_session(screen->portal, XdpOutputType::XDP_OUTPUT_MONITOR,
+	XdpScreencastFlags::XDP_SCREENCAST_FLAG_NONE, XdpCursorMode::XDP_CURSOR_MODE_METADATA,
+	XdpPersistMode::XDP_PERSIST_MODE_TRANSIENT, nullptr, nullptr, callback, screen);
+
 	return screen;
 }
 void screen_reader_destroy(ScreenReader *screen) {
 	if (screen->surface) cairo_surface_destroy(screen->surface);
+	pw_context_destroy(screen->pwContext);
+	pw_thread_loop_stop(screen->pwLoop);
+	pw_thread_loop_destroy(screen->pwLoop);
+	g_object_unref(screen->portal);
 	delete screen;
 }
 void screen_reader_add_rect(ScreenReader *screen, GdkScreen *gdkScreen, math::Rectangle<int> &rect) {
@@ -77,6 +134,14 @@ void screen_reader_update_surface(ScreenReader *screen, math::Rectangle<int> *up
 	cairo_destroy(cr);
 	cairo_destroy(rootCairo);
 	*updateRect = screen->readArea;
+
+	if(!screen->pwStream) return;
+	pw_thread_loop_lock(screen->pwLoop);
+	pw_buffer* buff = pw_stream_dequeue_buffer(screen->pwStream);
+	std::cout<<pw_stream_state_as_string(pw_stream_get_state(screen->pwStream,nullptr))<<std::endl;
+	std::cout<<buff<<std::endl;
+	pw_thread_loop_unlock(screen->pwLoop);
+
 }
 cairo_surface_t *screen_reader_get_surface(ScreenReader *screen) {
 	return screen->surface;
